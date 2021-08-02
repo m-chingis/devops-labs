13b2dca32a00e7ee260417779ead9c61ab014a13
Shyngys Meirmanov <m.chingis@gmail.com>
2021-08-02 19:49:02 +0600

    New Terraform config with 1 master and 3 worker nodes. Kubespray based k8s cluster was deployed after

--- tf/main.tf
+++ tf/main.tf
@@ -1,38 +1,39 @@
-# create pool
-resource "libvirt_pool" "ubuntu" {
-  name = "ubuntu-pool"
+########## Item: 01 ################
+# create pool for: node-cp1
+resource "libvirt_pool" "node-cp1" {
+  name = "pool-node-cp1"
   type = "dir"
-  path = "${var.vm_location}/ubuntu"
+  path = "${var.vm_location}/node-cp1"
 }
 
-# create image
-resource "libvirt_volume" "image-qcow2" {
-  name = "ubuntu-amd64.qcow2"
-  pool = libvirt_pool.ubuntu.name
+# create image for: node-cp1
+resource "libvirt_volume" "node-cp1-image-qcow2" {
+  name = "node-cp1-amd64.qcow2"
+  pool = libvirt_pool.node-cp1.name
   source = var.ubuntu_cloudimg_amd64_path 
   format = "qcow2"
 }
 
-# add cloudinit disk to pool
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name = "commoninit.iso"
-  pool = libvirt_pool.ubuntu.name
-  user_data = data.template_file.user_data.rendered
+# add cloudinit disk to pool for: node-cp1
+resource "libvirt_cloudinit_disk" "cloudinit-node-cp1" {
+  name = "cloudinit-node-cp1.iso"
+  pool = libvirt_pool.node-cp1.name
+  user_data = data.template_file.user_data-node-cp1.rendered
 }
 
-# read the configuration
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
+# read the configuration for: node-cp1
+data "template_file" "user_data-node-cp1" {
+  template = file("${path.module}/cloud_init-node-cp1.cfg")
 }
 
-# Define KVM domain to create
-resource "libvirt_domain" "ubuntu-vm1" {
+# Define KVM domain to create for: vm-node-cp1
+resource "libvirt_domain" "vm-cp1" {
   # name should be unique!
-  name = "ubuntu-vm1"
-  memory = "2048"
-  vcpu = 2
+  name = "vm-node-cp1"
+  memory = "1600"
+  vcpu = 1
   # add the cloud init disk to share user data
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.cloudinit-node-cp1.id
   # set to default libvirt network
   network_interface {
     network_name = "default"
@@ -43,7 +44,174 @@ resource "libvirt_domain" "ubuntu-vm1" {
     target_port = "0"
   }
   disk {
-    volume_id = libvirt_volume.image-qcow2.id
+    volume_id = libvirt_volume.node-cp1-image-qcow2.id
+  }
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+########## Item: 02 ################
+# create pool for: node-w01
+resource "libvirt_pool" "node-w01" {
+  name = "pool-node-w01"
+  type = "dir"
+  path = "${var.vm_location}/node-w01"
+}
+
+# create image for: node-w01
+resource "libvirt_volume" "node-w01-image-qcow2" {
+  name = "node-w01-amd64.qcow2"
+  pool = libvirt_pool.node-w01.name
+  source = var.ubuntu_cloudimg_amd64_path
+  format = "qcow2"
+}
+
+# add cloudinit disk to pool for: node-w01
+resource "libvirt_cloudinit_disk" "cloudinit-node-w01" {
+  name = "cloudinit-node-w01.iso"
+  pool = libvirt_pool.node-w01.name
+  user_data = data.template_file.user_data-node-w01.rendered
+}
+
+# read the configuration for: node-w01
+data "template_file" "user_data-node-w01" {
+  template = file("${path.module}/cloud_init-node-w01.cfg")
+}
+
+# Define KVM domain to create for: node-w01
+resource "libvirt_domain" "vm-node-w01" {
+  # name should be unique!
+  name = "vm-node-w01"
+  memory = "1100"
+  vcpu = 1
+  # add the cloud init disk to share user data
+  cloudinit = libvirt_cloudinit_disk.cloudinit-node-w01.id
+  # set to default libvirt network
+  network_interface {
+    network_name = "default"
+  }
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+  disk {
+    volume_id = libvirt_volume.node-w01-image-qcow2.id
+  }
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+
+########## Item: 03 ################
+# create pool for: node-w02
+resource "libvirt_pool" "node-w02" {
+  name = "pool-node-w02"
+  type = "dir"
+  path = "${var.vm_location}/node-w02"
+}
+
+# create image for: node-w02
+resource "libvirt_volume" "node-w02-image-qcow2" {
+  name = "node-w02-amd64.qcow2"
+  pool = libvirt_pool.node-w02.name
+  source = var.ubuntu_cloudimg_amd64_path
+  format = "qcow2"
+}
+
+# add cloudinit disk to pool for: node-w02
+resource "libvirt_cloudinit_disk" "cloudinit-node-w02" {
+  name = "cloudinit-node-w02.iso"
+  pool = libvirt_pool.node-w02.name
+  user_data = data.template_file.user_data-node-w02.rendered
+}
+
+# read the configuration for: node-w02
+data "template_file" "user_data-node-w02" {
+  template = file("${path.module}/cloud_init-node-w02.cfg")
+}
+
+# Define KVM domain to create for: node-w02
+resource "libvirt_domain" "vm-node-w02" {
+  # name should be unique!
+  name = "vm-node-w02"
+  memory = "1100"
+  vcpu = 1
+  # add the cloud init disk to share user data
+  cloudinit = libvirt_cloudinit_disk.cloudinit-node-w02.id
+  # set to default libvirt network
+  network_interface {
+    network_name = "default"
+  }
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+  disk {
+    volume_id = libvirt_volume.node-w02-image-qcow2.id
+  }
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+
+########## Item: 04 ################
+# create pool for: node-w03
+resource "libvirt_pool" "node-w03" {
+  name = "pool-node-w03"
+  type = "dir"
+  path = "${var.vm_location}/node-w03"
+}
+
+# create image for: node-w03
+resource "libvirt_volume" "node-w03-image-qcow2" {
+  name = "node-w03-amd64.qcow2"
+  pool = libvirt_pool.node-w03.name
+  source = var.ubuntu_cloudimg_amd64_path
+  format = "qcow2"
+}
+
+# add cloudinit disk to pool for: node-w03
+resource "libvirt_cloudinit_disk" "cloudinit-node-w03" {
+  name = "cloudinit-node-w03.iso"
+  pool = libvirt_pool.node-w03.name
+  user_data = data.template_file.user_data-node-w03.rendered
+}
+
+# read the configuration for: node-w03
+data "template_file" "user_data-node-w03" {
+  template = file("${path.module}/cloud_init-node-w03.cfg")
+}
+
+# Define KVM domain to create for: node-w03
+resource "libvirt_domain" "vm-node-w03" {
+  # name should be unique!
+  name = "vm-node-w03"
+  memory = "1100"
+  vcpu = 1
+  # add the cloud init disk to share user data
+  cloudinit = libvirt_cloudinit_disk.cloudinit-node-w03.id
+  # set to default libvirt network
+  network_interface {
+    network_name = "default"
+  }
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+  disk {
+    volume_id = libvirt_volume.node-w03-image-qcow2.id
   }
   graphics {
     type = "spice"
